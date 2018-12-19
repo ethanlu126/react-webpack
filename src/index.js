cc6aec108f4ad16e2e5f6669e66fc6f513077dc6
Ethan Lu <lujingyi@codemao.cn>
2018-12-19 18:21:39 +0800

    update .gitignore

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// console.log("aaabb");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const testdiv = <div id="test" title="testdiv">axacacaca</div> 
+
+ReactDOM.render(testdiv, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+const htmlPlug = new HTMLWebPackPlugin(
+  {
+    template: path.join(__dirname, './src/index.html'),
+    filename: 'index.html'
+  }
+);
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {test: /\.js|jsx$/ , use: 'babel-loader', exclude: /node_modules/} 
+    ]
+  },
+  plugins: [
+    htmlPlug
+  ]
+};
