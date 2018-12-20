fe30b1e0d5efc13eb47cebec968dfea9e39f2a4d
Ethan Lu <ethanlu126@gmail.com>
2018-12-20 17:44:49 +0800

    feat: learning how to render a function as div

--- src/index.js
+++ src/index.js
@@ -4,17 +4,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // const testdiv = <div id="test" title="testdiv">axacacaca</div>
-
-
+//P20
 const language = ['js', 'ts', 'c#', 'c++'];
-
+//
+const learningOBJ = {
+    frontend: 'react',
+    pack: 'webpack'
+}
+function LEARNING(props) {
+    console.log(props);
+    return <div id="LEARNING">LEARNING frontend: {props.frontend} pack: {props.pack}</div>
+}
 
 ReactDOM.render(
     <div id="test" title="test title">
-        {
+        {/* {
             language.map(item => {
-                return <h1>{item}</h1>
+                return <h3 key={language.indexOf(item)}>{item}</h3>
             })
-        }
+        } */}
+        <LEARNING frontend={learningOBJ.frontend} pack={learningOBJ.pack}></LEARNING>
     </div>,
     document.getElementById('app'));
