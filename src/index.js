07ea38d4368a1c72fc4c69eab7c894991c5ae8ca
Ethan Lu <ethanlu126@gmail.com>
2018-12-20 10:57:50 +0800

    feat: learning how to render jsx array

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const testdiv = <div id="test" title="testdiv">axacacaca</div> 
+// const testdiv = <div id="test" title="testdiv">axacacaca</div>
 
-ReactDOM.render(testdiv, document.getElementById('app'));
+
+const language = ['js', 'ts', 'c#', 'c++'];
+
+
+ReactDOM.render(
+    <div id="test" title="test title">
+        {
+            language.map(item => {
+                return <h1>{item}</h1>
+            })
+        }
+    </div>,
+    document.getElementById('app'));
