53bf480cec29787f1e59ca890a69cdf0a9f633f1
Ethan Lu <ethanlu126@gmail.com>
2019-01-05 23:39:32 +0800

    feat: learning how to append state data on class

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
 export class Movie extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: 'IMDb'
+        }
+    }
     render() {
         return <div>
             Top Rated Movies : <br/>
             1.{this.props.first} <br/>
             2.{this.props.second} <br/>
-            3.{this.props.third}
+            3.{this.props.third} <br/>
+            from {this.state.title}
         </div>
     }
 }
