eaaf8173131a62f2d4c220864fdd67d088b23292
Ethan Lu <ethanlu126@gmail.com>
2019-01-28 15:38:15 +0800

    feat: learning add style name from css path and name

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.js|jsx$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: ['style-loader', 'css-loader?modules']}
+      { test: /\.css$/, use: ['style-loader', 'css-loader?modules&localIdentName=[path][name]-[local]-[hash:6]']}
     ]
   },
   plugins: [
