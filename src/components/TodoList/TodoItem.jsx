f513e9cb5157320bc8986d87277177b288d3364b
Ethan Lu <ethanlu126@gmail.com>
2019-02-14 11:27:35 +0800

    feat: learning propTypes

--- src/components/TodoList/TodoItem.jsx
+++ src/components/TodoList/TodoItem.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+import TodoList from './TodoList';
 
 class TodoItem extends React.Component {
     constructor(props) {
@@ -21,6 +23,16 @@ class TodoItem extends React.Component {
         const { index, handleDeleteItem } = this.props;
         handleDeleteItem(this.props.index);
     }
+}
+// check props type
+TodoItem.propTypes = {
+    test: PropTypes.string,
+    index: PropTypes.number
+}
+
+// set default value of props (if props were undefined)
+TodoList.defaultProps = {
+    test: 'test'
 }
 
 export default TodoItem;
